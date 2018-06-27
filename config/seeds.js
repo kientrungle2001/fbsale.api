c69f2007251b1ae8abcdd10d23da5090d44db0eb
Le Trung Kien <kientrungle2001@gmail.com>
2018-06-27 15:18:20 +0700

    seeds

---
+++ config/seeds.js
@@ -0,0 +1,20 @@
+/**
+ * Sails Seed Settings
+ * (sails.config.seeds)
+ *
+ * Configuration for the data seeding in Sails.
+ *
+ * For more information on configuration, check out:
+ * http://github.com/frostme/sails-seed
+ */
+module.exports.seeds = {
+	user: [
+	{
+		name: 'Lê Trung Kiên',
+		email: 'kientrungle2001@gmail.com',
+		phone: '01647867486',
+		username: 'kientrungle2001',
+		status: 1
+	}
+	]
+}
