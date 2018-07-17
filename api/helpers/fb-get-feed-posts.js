d3316622925cedb7023e6702cba1d3d01d8b2c75
Le Trung Kien <kientrungle2001@gmail.com>
2018-07-17 16:05:24 +0700

    get feed

---
+++ api/helpers/fb-get-feed-posts.js
@@ -0,0 +1,43 @@
+var request = require('request');
+
+module.exports = {
+
+  friendlyName: 'fb Get Posts',
+
+
+  description: 'Trả về danh sách các bài viết của 1 page ',
+
+
+  inputs: {    
+    pageId: {
+      type: 'string',
+      example: 'page_id',
+      description: 'id của page',
+      required: true
+    },
+    token: {
+      type: 'string',
+      example: 'access_token',
+      description: 'access_token',
+      required: true
+    }
+
+  },
+  // Link test ( method: GET): /2125248527733637/feed
+  fn: async function (inputs, exits) {
+    // Lấy danh sách các bài viết của page
+           var page_url=  'https://graph.facebook.com/v3.0/'+inputs.pageId+'/feed?fields=likes{id,name,pic_large},comments{comments{id,message,attachment,likes{id,name,pic_large},created_time,from},id,message,attachment,likes{id,name,pic_large},from},id,message,full_picture&access_token='+inputs.token;
+           var page_options = {method: 'GET', url: page_url, json: true};
+           request(page_options, function (err, response) {
+           
+            if (err) {
+              //return 'null';
+              return exits.success(err);
+            }
+
+            var posts_data = response.body.data;
+            return exits.success(posts_data);
+          });
+  }
+
+};
