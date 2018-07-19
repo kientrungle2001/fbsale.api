3494609f0e0be8955c89aeef64d1e83a819a0159
kieunghia121310 <kieunghia.cntt@gmail.com>
2018-07-19 15:44:05 +0700

    commit

--- api/helpers/fb-post-comments.js
+++ api/helpers/fb-post-comments.js
@@ -33,8 +33,9 @@ module.exports = {
 
   fn: async function (inputs, exits) {
     // Post comments, return comment_id
-           var page_url=  'https://graph.facebook.com/v3.0/'+inputs.postId+'/comments?message='+inputs.comment+'&access_token='+inputs.token;
-           var page_options = {method: 'POST', url: page_url, json: true};
+           var data_post ={"message":inputs.comment};
+           var page_url=  'https://graph.facebook.com/v3.0/'+inputs.postId+'/comments?access_token='+inputs.token;
+           var page_options = {method: 'POST', url: page_url, json: data_post};
            request(page_options, function (err, response) {
             if (err) {
               //return 'null';
