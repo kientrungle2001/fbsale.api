6a6602cafba94fc905bf2ae544470c92f19b2bf9
Le Trung Kien <kientrungle2001@gmail.com>
2018-07-24 15:08:56 +0700

    label

--- api/models/SocialPostLabels.js
+++ api/models/SocialPostLabels.js
@@ -2,14 +2,6 @@ module.exports = {
   tableName: 'social_post_labels',
   attributes: {
 
-    post_id: {
-        type: 'string',
-        columnType: 'varchar'
-    },
-    label_id: {
-        type: 'number',
-        columnType: 'int'
-    },
     // Khai bao lien ket toi models CoreLabels
     label_id: {
     	model: 'CoreLabels'        
@@ -18,7 +10,6 @@ module.exports = {
     // Ket noi giua model SocilaPostLabels voi model SocialPosts
     post_id: {
         model: 'SocialPosts',
-        unique: true
     }
   },
   
